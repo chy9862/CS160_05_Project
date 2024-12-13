a87a84ad97b63b4e92c69511ba7c67cf0bdadfdf
Hyun Woo Kim <106051666+chy9862@users.noreply.github.com>
2024-12-12 16:51:50 -0800

    Update FoodItemCard.jsx

--- components/FoodItemCard/FoodItemCard.jsx
+++ components/FoodItemCard/FoodItemCard.jsx
@@ -5,6 +5,12 @@ import {checkImageURL} from '@/utils/index.js'
 
 // Create a card object that when pressed, sends you to the item page. The card displays the item name, expiration date, and the item image
 const FoodItemCard = ({ item, selectedItem, handleCardPress }) => {
+    
+    const defaultImageURL = 'https://img.freepik.com/premium-vector/fork-spoon-logo-design-icon-symbol-health-restaurant-food_1041562-261.jpg';
+    
+    const imageSource = item.imageURL && item.imageURL.trim() !== ''
+        ? { uri: item.imageURL } 
+        : { uri: defaultImageURL }; 
     return (
         // Make the card pressable and handle card press action with the passed in handleCardPress function from FoodItem.jsx
         <TouchableOpacity
@@ -15,10 +21,7 @@ const FoodItemCard = ({ item, selectedItem, handleCardPress }) => {
                 <TouchableOpacity style={styles.logoContainer(selectedItem, item)}>
                     {/* Display the product's image from API database. If the product does not have an image, use a default image */}
                     <Image 
-                        source={{ uri: checkImageURL(item.imageURL)
-                            ? item.imageURL 
-                            : 'https://img.freepik.com/premium-vector/fork-spoon-logo-design-icon-symbol-health-restaurant-food_1041562-261.jpg'
-                        }}
+                        source={imageSource}
                         resizeMode="contain"
                         style={styles.logoImage}
                     />
@@ -33,7 +36,7 @@ const FoodItemCard = ({ item, selectedItem, handleCardPress }) => {
                     <View style={styles.infoWrapper}>
                         {/* Display expiration date */}
                         <Text style={styles.expiration(selectedItem, item)}>
-                            {item.expirationDate} 
+                            Exp Date: {item.expirationDate} 
                         </Text>
                     </View>
                 </View>
